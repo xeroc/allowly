35dc582422e8a6f3d479d39babcc39a7492cf9fb
Fabian Schuh <mail@xeroc.org>
2026-01-16 18:19:14 +0000

    🐛 fix: more build issues

--- packages/app/components/PolicyCard.tsx
+++ packages/app/components/PolicyCard.tsx
@@ -163,7 +163,6 @@ export function PolicyCard({ policy }: PolicyCardProps) {
               initial="rest"
               whileHover="hover"
               whileTap="tap"
-              disabled={isLoading}
             >
               <svg
                 className="w-4 h-4"
@@ -189,7 +188,6 @@ export function PolicyCard({ policy }: PolicyCardProps) {
             initial="rest"
             whileHover="hover"
             whileTap="tap"
-            disabled={isLoading}
           >
             <svg
               className="w-4 h-4"
